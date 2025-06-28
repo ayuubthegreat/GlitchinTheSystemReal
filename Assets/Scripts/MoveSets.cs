23668d1b1cb75720c3379ac72b2314347979ff02
ayuubthegreat <yusufayuub00@gmail.com>
2025-06-28 14:08:26 -0500

    Create MoveSets.cs

---
+++ Assets/Scripts/MoveSets.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class NewScript : MonoBehaviour
+{
+public struct moveSet{
+
+
+}
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Initialization code here
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Code to execute every frame here
+    }
+}
